ab2709669ca6dcb51d59ba7d97f3e1a3089f42eb
Diana Cretu <diana.cretu@nxp.com>
2020-01-17 14:59:46 +0200

    hw: adsp: Add initial support for i.MX8
    Signed-off-by: Diana Cretu <diana.cretu@nxp.com>

---
+++ target/xtensa/core-imx8.c
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2015, Intel Corporation.
+ * Copyright 2020 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Open Source and Linux Lab nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "exec/exec-all.h"
+#include "exec/gdbstub.h"
+#include "qemu/host-utils.h"
+
+#include "core-imx8/core-isa.h"
+#include "overlay_tool.h"
+
+typedef struct {
+  int reg_num;
+  int bit_start;
+  int bit_size;
+} xtensa_reg_mask_t;
+
+typedef struct {
+  int count;
+  xtensa_reg_mask_t *mask;
+} xtensa_mask_t;
+
+#if 0
+/* Masked registers.  */
+xtensa_reg_mask_t xtensa_submask0[] = { { 44, 0, 1 } };
+const xtensa_mask_t xtensa_mask0 = { 1, xtensa_submask0 };
+xtensa_reg_mask_t xtensa_submask1[] = { { 44, 1, 1 } };
+const xtensa_mask_t xtensa_mask1 = { 1, xtensa_submask1 };
+xtensa_reg_mask_t xtensa_submask2[] = { { 44, 2, 1 } };
+const xtensa_mask_t xtensa_mask2 = { 1, xtensa_submask2 };
+xtensa_reg_mask_t xtensa_submask3[] = { { 44, 3, 1 } };
+const xtensa_mask_t xtensa_mask3 = { 1, xtensa_submask3 };
+xtensa_reg_mask_t xtensa_submask4[] = { { 44, 4, 1 } };
+const xtensa_mask_t xtensa_mask4 = { 1, xtensa_submask4 };
+xtensa_reg_mask_t xtensa_submask5[] = { { 44, 5, 1 } };
+const xtensa_mask_t xtensa_mask5 = { 1, xtensa_submask5 };
+xtensa_reg_mask_t xtensa_submask6[] = { { 44, 6, 1 } };
+const xtensa_mask_t xtensa_mask6 = { 1, xtensa_submask6 };
+xtensa_reg_mask_t xtensa_submask7[] = { { 44, 7, 1 } };
+const xtensa_mask_t xtensa_mask7 = { 1, xtensa_submask7 };
+xtensa_reg_mask_t xtensa_submask8[] = { { 44, 8, 1 } };
+const xtensa_mask_t xtensa_mask8 = { 1, xtensa_submask8 };
+xtensa_reg_mask_t xtensa_submask9[] = { { 44, 9, 1 } };
+const xtensa_mask_t xtensa_mask9 = { 1, xtensa_submask9 };
+xtensa_reg_mask_t xtensa_submask10[] = { { 44, 10, 1 } };
+const xtensa_mask_t xtensa_mask10 = { 1, xtensa_submask10 };
+xtensa_reg_mask_t xtensa_submask11[] = { { 44, 11, 1 } };
+const xtensa_mask_t xtensa_mask11 = { 1, xtensa_submask11 };
+xtensa_reg_mask_t xtensa_submask12[] = { { 44, 12, 1 } };
+const xtensa_mask_t xtensa_mask12 = { 1, xtensa_submask12 };
+xtensa_reg_mask_t xtensa_submask13[] = { { 44, 13, 1 } };
+const xtensa_mask_t xtensa_mask13 = { 1, xtensa_submask13 };
+xtensa_reg_mask_t xtensa_submask14[] = { { 44, 14, 1 } };
+const xtensa_mask_t xtensa_mask14 = { 1, xtensa_submask14 };
+xtensa_reg_mask_t xtensa_submask15[] = { { 44, 15, 1 } };
+const xtensa_mask_t xtensa_mask15 = { 1, xtensa_submask15 };
+xtensa_reg_mask_t xtensa_submask16[] = { { 42, 0, 4 } };
+const xtensa_mask_t xtensa_mask16 = { 1, xtensa_submask16 };
+xtensa_reg_mask_t xtensa_submask17[] = { { 42, 5, 1 } };
+const xtensa_mask_t xtensa_mask17 = { 1, xtensa_submask17 };
+xtensa_reg_mask_t xtensa_submask18[] = { { 42, 18, 1 } };
+const xtensa_mask_t xtensa_mask18 = { 1, xtensa_submask18 };
+xtensa_reg_mask_t xtensa_submask19[] = { { 42, 4, 1 } };
+const xtensa_mask_t xtensa_mask19 = { 1, xtensa_submask19 };
+xtensa_reg_mask_t xtensa_submask20[] = { { 42, 16, 2 } };
+const xtensa_mask_t xtensa_mask20 = { 1, xtensa_submask20 };
+xtensa_reg_mask_t xtensa_submask21[] = { { 42, 8, 4 } };
+const xtensa_mask_t xtensa_mask21 = { 1, xtensa_submask21 };
+xtensa_reg_mask_t xtensa_submask22[] = { { 112, 8, 4 } };
+const xtensa_mask_t xtensa_mask22 = { 1, xtensa_submask22 };
+xtensa_reg_mask_t xtensa_submask23[] = { { 47, 7, 1 } };
+const xtensa_mask_t xtensa_mask23 = { 1, xtensa_submask23 };
+xtensa_reg_mask_t xtensa_submask24[] = { { 47, 0, 7 }, { 47, 8, 7 } };
+const xtensa_mask_t xtensa_mask24 = { 2, xtensa_submask24 };
+xtensa_reg_mask_t xtensa_submask25[] = { { 50, 28, 1 } };
+const xtensa_mask_t xtensa_mask25 = { 1, xtensa_submask25 };
+xtensa_reg_mask_t xtensa_submask26[] = { { 49, 0, 4 } };
+const xtensa_mask_t xtensa_mask26 = { 1, xtensa_submask26 };
+xtensa_reg_mask_t xtensa_submask27[] = { { 49, 4, 4 } };
+const xtensa_mask_t xtensa_mask27 = { 1, xtensa_submask27 };
+xtensa_reg_mask_t xtensa_submask28[] = { { 49, 12, 4 } };
+const xtensa_mask_t xtensa_mask28 = { 1, xtensa_submask28 };
+xtensa_reg_mask_t xtensa_submask29[] = { { 49, 8, 4 } };
+const xtensa_mask_t xtensa_mask29 = { 1, xtensa_submask29 };
+xtensa_reg_mask_t xtensa_submask30[] = { { 50, 0, 27 } };
+const xtensa_mask_t xtensa_mask30 = { 1, xtensa_submask30 };
+xtensa_reg_mask_t xtensa_submask31[] = { { 50, 27, 1 } };
+const xtensa_mask_t xtensa_mask31 = { 1, xtensa_submask31 };
+xtensa_reg_mask_t xtensa_submask32[] = { { 79, 5, 2 } };
+const xtensa_mask_t xtensa_mask32 = { 1, xtensa_submask32 };
+xtensa_reg_mask_t xtensa_submask33[] = { { 79, 4, 1 } };
+const xtensa_mask_t xtensa_mask33 = { 1, xtensa_submask33 };
+xtensa_reg_mask_t xtensa_submask34[] = { { 79, 3, 1 } };
+const xtensa_mask_t xtensa_mask34 = { 1, xtensa_submask34 };
+xtensa_reg_mask_t xtensa_submask35[] = { { 79, 2, 1 } };
+const xtensa_mask_t xtensa_mask35 = { 1, xtensa_submask35 };
+xtensa_reg_mask_t xtensa_submask36[] = { { 79, 1, 1 } };
+const xtensa_mask_t xtensa_mask36 = { 1, xtensa_submask36 };
+xtensa_reg_mask_t xtensa_submask37[] = { { 79, 0, 1 } };
+const xtensa_mask_t xtensa_mask37 = { 1, xtensa_submask37 };
+#endif
+
+static XtensaConfig imx8 __attribute__((unused)) = {
+    .name = "imx8",
+    .gdb_regmap = {
+        .num_regs = 174,
+        .num_core_regs = 52,
+        .reg = {
+#include "core-imx8/gdb-config.c"
+        }
+    },
+    .clock_freq_khz = 5000,
+    DEFAULT_SECTIONS
+};
+
+REGISTER_CORE(imx8)
